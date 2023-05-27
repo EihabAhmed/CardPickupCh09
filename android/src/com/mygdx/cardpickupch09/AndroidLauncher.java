1e306b245fcd55e4d5798762fbfc8fb60b6c9bbe
EihabAhmed <eihabahmed@gmail.com>
2023-05-27 23:24:29 +0300

    Initial Commit

---
+++ android/src/com/mygdx/cardpickupch09/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.cardpickupch09;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.cardpickupch09.CardPickupGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CardPickupGame(), config);
+	}
+}
